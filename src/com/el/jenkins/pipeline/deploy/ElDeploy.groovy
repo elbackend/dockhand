a26fd403840c5f553c150e2b8cc389749c1a9025
jesse <jesse@antoszyk.com>
2019-09-11 12:24:05 -0400

    deploy logic

--- src/com/el/jenkins/pipeline/deploy/ElDeploy.groovy
+++ src/com/el/jenkins/pipeline/deploy/ElDeploy.groovy
@@ -1,11 +1,34 @@
 package com.el.jenkins.pipeline.deploy
 
 import com.boxboat.jenkins.pipeline.deploy.BoxDeploy
+import com.boxboat.jenkins.pipeline.deploy.kubernetes.HelmDeploy
 
 class ElDeploy extends BoxDeploy {
 
     ElDeploy(Map config = [:]) {
         super(config)
         setPropertiesFromMap(config)
+    }
+
+    public deploy() {
+      this.writeImageTags(
+        outFile: "./cicd/deploy/${app}/image-tags.yaml",
+      )
+
+      def releaseName = "${app}-${this.config.deploymentKey}"
+      this.allEnvironments().each { env ->
+            def helmDeployApp = new HelmDeploy(
+                chart: ".",
+                directory: "./cicd/deploy/${app}",
+                name: releaseName,
+                options: [
+                    "set"      : [],
+                    "values"   : [
+                        "image-tags.yaml",
+                        "values-${env.name}.yaml",
+                    ],
+                ]
+            )
+        }
     }
 }
