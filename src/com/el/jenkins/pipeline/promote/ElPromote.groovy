bf32998d0bed1dbb949366617596f5f2952d7487
jesse <jesse@antoszyk.com>
2019-09-20 08:31:01 -0400

    promotion keys

--- src/com/el/jenkins/pipeline/promote/ElPromote.groovy
+++ src/com/el/jenkins/pipeline/promote/ElPromote.groovy
@@ -1,10 +1,10 @@
 package com.el.jenkins.pipeline.promote
 
-import com.boxboat.jenkins.pipeline.deploy.BoxPromote
+import com.boxboat.jenkins.pipeline.promote.BoxPromote
 
 class ElPromote extends BoxPromote {
 
-    ElDeploy(Map config = [:]) {
+    ElPromote(Map config = [:]) {
         super(config)
     }
 }
--- vars/elPromoteProps.groovy
+++ vars/elPromoteProps.groovy
@@ -1,5 +1,13 @@
 def props() {
+  def promotions = [
+            "",
+            "stage",
+            "prod",
+    ]
     return [
+            parameters([
+                    choice(name: 'promotionKey', choices: promotions, description: 'Promotion', defaultValue: ''),
+            ]),
             [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', numToKeepStr: '100']]
     ]
 
